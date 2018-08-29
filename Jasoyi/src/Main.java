6a55b22d042d8514c2d27ddb0c1bc92a0e9b2c19
AbeyanceCoding <abey5403@gmail.com>
2018-08-29 17:39:14 -0400

    Add files via upload

---
+++ Jasoyi/src/Main.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Jasoyi");
+		frame.setVisible(true);
+		frame.setSize(640, 640);
+		frame.setResizable(false);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
